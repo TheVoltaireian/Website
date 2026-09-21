c9c6f41185ebbfcc22a22dab1ba5f775e8a30806
WildVoltaireian <dostalyavoltaire@gmail.com>
2026-09-21 19:37:35 -0400

    modified:   assets/js/main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,6 +39,33 @@ document.querySelectorAll('.projects__cardHead').forEach(head => {
     resizeObserver.observe(head);
 });
 
+let mouseX = 0;
+let mouseY = 0;
+
+let lerpedX = 0;
+let lerpedY = 0;
+
+document.addEventListener("mousemove", e => {
+    mouseX = e.clientX / window.innerWidth * 20 - 10;
+    mouseY = e.clientY / window.innerHeight * 20 - 10;
+});
+
+const tweets = document.querySelectorAll("#WildVoltaireian");
+
+function update() {
+    lerpedX += (mouseX - lerpedX) * 0.05;
+    lerpedY += (mouseY - lerpedY) * 0.05;
+
+    tweets.forEach(tweet => {
+        tweet.style.transform =
+            `translate(${lerpedX}px, ${lerpedY}px) rotate(${lerpedX * 0.15}deg)`;
+    });
+
+    requestAnimationFrame(update);
+}
+
+update();
+
 console.log(String.raw`
  /$$      /$$ /$$ /$$       /$$ /$$    /$$          /$$   /$$               /$$                     /$$                    
 | $$  /$ | $$|__/| $$      | $$| $$   | $$         | $$  | $$              |__/                    |__/                    
